a7e47e84788eef97fa9b0f71e7e406e627c51b03
Rahim <doomsdayrs@gmail.com>
2018-10-30 09:17:53 -0400

    Added header

--- src/main/java/com/github/Doomsdayrs/Jikan4java/connection/Person/PersonConnection.java
+++ src/main/java/com/github/Doomsdayrs/Jikan4java/connection/Person/PersonConnection.java
@@ -1,4 +1,26 @@
 package com.github.Doomsdayrs.Jikan4java.connection.Person;
+/**
+ * Jikan4java
+ * 28 / October / 2018
+ *
+ * @author github.com/doomsdayrs
+ */
+/*
+This file is part of Jikan4java.
+
+Jikan4java is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jikan4java.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.Doomsdayrs.Jikan4java.types.Main.Manga.Manga;
@@ -12,6 +34,7 @@ import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 public class PersonConnection {
     private final OkHttpClient client = new OkHttpClient();
