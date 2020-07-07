6e14ed5b1aee7471f73df8fc5baf6dcb43be602a
doomsdayrs <doomsdayrs@gmail.com>
2020-07-07 19:35:50 -0400

    - model/main/meta/* turned kotlin

--- src/main/java/com/github/doomsdayrs/jikan4java/model/main/meta/Status.kt
+++ src/main/java/com/github/doomsdayrs/jikan4java/model/main/meta/Status.kt
@@ -1,6 +1,6 @@
-package com.github.doomsdayrs.jikan4java.model.main.meta;
+package com.github.doomsdayrs.jikan4java.model.main.meta
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty
 
 /*
  * This file is part of Jikan4java.
@@ -23,29 +23,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  *
  * @author github.com/doomsdayrs
  */
-public class Status {
-    @JsonProperty("cached_requests")
-    public int cached_requests;
-    @JsonProperty("requests_today")
-    public int requests_today;
-    @JsonProperty("requests_this_week")
-    public int requests_this_week;
-    @JsonProperty("requests_this_month")
-    public int requests_this_month;
-    @JsonProperty("connected_clients")
-    public String connected_clients;
-    @JsonProperty("total_connections_received")
-    public String total_connections_received;
-
-    @Override
-    public String toString() {
-        return "Status{" +
-                "cached_requests=" + cached_requests +
-                ", requests_today=" + requests_today +
-                ", requests_this_week=" + requests_this_week +
-                ", requests_this_month=" + requests_this_month +
-                ", connected_clients='" + connected_clients + '\'' +
-                ", total_connections_received='" + total_connections_received + '\'' +
-                '}';
-    }
-}
+data class Status(
+		@field:JsonProperty("cached_requests") val cached_requests: Int = 0,
+		@field:JsonProperty("requests_today") val requests_today: Int = 0,
+		@field:JsonProperty("requests_this_week") val requests_this_week: Int = 0,
+		@field:JsonProperty("requests_this_month") val requests_this_month: Int = 0,
+		@field:JsonProperty("connected_clients") val connected_clients: String? = null,
+		@field:JsonProperty("total_connections_received") val total_connections_received: String? = null
+)
