8f41124eb182ed9bcc740bc04a38f6400d73119d
doomsdayrs <doomsdayrs@gmail.com>
2021-04-17 13:13:40 -0400

    - MultiPlatform gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,65 @@
 plugins {
-	java
-	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.serialization") version "1.4.21"
+	kotlin("multiplatform") version "1.5.0-RC"
 }
 
 group = "com.github.doomsdayrs"
-version = "2.0.0"
+version = "3.0.0"
 description = "jikan4java"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenLocal()
 	mavenCentral()
-	maven("https://jitpack.io")
-	maven("https://repo.maven.apache.org/maven2/")
 }
 
-dependencies {
-	implementation("com.squareup.okhttp3:okhttp:4.9.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.21")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.2")
+kotlin {
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+		testRuns["test"].executionTask.configure {
+			useJUnit()
+		}
+	}
+	js(LEGACY) {
+		browser {
+			commonWebpackConfig {
+				cssSupport.enabled = true
+			}
+		}
+	}
+	val hostOs = System.getProperty("os.name")
+	val isMingwX64 = hostOs.startsWith("Windows")
+	val nativeTarget = when {
+		hostOs == "Mac OS X" -> macosX64("native")
+		hostOs == "Linux" -> linuxX64("native")
+		isMingwX64 -> mingwX64("native")
+		else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+	}
 
-	testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.21")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.4.21")
 
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-	kotlinOptions.jvmTarget = "1.8"
+	sourceSets {
+		val commonMain by getting
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test-common"))
+				implementation(kotlin("test-annotations-common"))
+				implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+			}
+		}
+		val jvmMain by getting
+		val jvmTest by getting {
+			dependencies {
+				implementation(kotlin("test-junit"))
+			}
+		}
+		val jsMain by getting
+		val jsTest by getting {
+			dependencies {
+				implementation(kotlin("test-js"))
+			}
+		}
+		val nativeMain by getting
+		val nativeTest by getting
+	}
 }
